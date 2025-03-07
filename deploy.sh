7a7fb8565a8637bebd59e200406bb10f893d37bb
Kelly Taylor <kelly.taylor@kibocommerce.com>
2025-03-07 15:35:43 -0800

    skip tests for deploy goal

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "MAVEN_OPTS=$MAVEN_OPTS PUBLISH=$PUBLISH"
 cat maven_settings.xml
 
 if [ "$PUBLISH" = "true" ]; then
-  mvn -B -e -s maven_settings.xml deploy -fn -P nexus-deploy
+  mvn -B -e -s maven_settings.xml deploy -fn -P nexus-deploy -DskipTests -DskipITs
 else
   echo "Skipping deployment as PUBLISH is not set to true."
 fi
